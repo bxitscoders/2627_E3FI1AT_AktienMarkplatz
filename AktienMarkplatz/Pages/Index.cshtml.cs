144087a629e0927c7037ade5f2631a772eac40a1
lebarth <l.barth@transnetbw.de>
2026-09-15 11:13:54 +0200

    Test DB connection

--- AktienMarkplatz/Pages/Index.cshtml.cs
+++ AktienMarkplatz/Pages/Index.cshtml.cs
@@ -1,13 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+using System.Text;
 
 namespace AktienMarkplatz.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string DbInhalt { get; private set; } = string.Empty;
+
         public void OnGet()
         {
+            var connectionString = _configuration.GetConnectionString("AktienMarkplatzDb");
 
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = "SELECT Id, Name FROM Test ORDER BY Id";
+
+            var sb = new StringBuilder();
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var id = reader.GetInt32(0);
+                var name = reader.GetString(1);
+                sb.AppendLine($"{id}: {name}");
+            }
+
+            DbInhalt = sb.Length > 0 ? sb.ToString() : "Keine Daten gefunden.";
         }
     }
 }
