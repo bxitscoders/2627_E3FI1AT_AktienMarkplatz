c90f404f70719bc2cc75c38d73c5487b2ca963c7
Tamino.Schinke <tamino.schinke@sidion.de>
2026-09-17 14:54:01 +0200

    Die Konfiguration festlegen

--- AktienMarkplatz/Program.cs
+++ AktienMarkplatz/Program.cs
@@ -1,6 +1,14 @@
+using CoreIdent.Core.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Der: SigningKeySecret wurde in user secrets gesichert
+builder.Services.AddCoreIdent(o => {
+    o.Issuer = "https://localhost:7280";
+    o.Audience = "https://localhost:7280/resources/my-api"; 
+    o.SigningKeySecret = builder.Configuration["CoreIdent:SigningKeySecret"];
+});
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
